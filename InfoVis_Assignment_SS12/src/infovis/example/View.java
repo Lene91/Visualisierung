4026134651f32bd324dc6bb701638cd7960c8091
VeronikaH <vronchen@gmail.com>
2014-04-23 17:34:57 +0200

    hihi

--- InfoVis_Assignment_SS12/src/infovis/example/View.java
+++ InfoVis_Assignment_SS12/src/infovis/example/View.java
@@ -22,9 +22,9 @@ public class View extends JPanel {
         
         
         Rectangle2D rect = new Rectangle2D.Double(50,50,200,200);
-        Rectangle2D rect2 = new Rectangle2D.Double(10,10,100,100);
+        Rectangle2D rect2 = new Rectangle2D.Double(50,50,100,100);
         
-        Color color1 = Color.BLUE;
+        Color color1 = Color.GREEN;
         
         g2D.setColor(color1);
         g2D.fill(rect);
